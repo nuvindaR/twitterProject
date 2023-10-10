2b99e3ef4f408827d6ffc945805fbb04fc8dbf5b
nuvindaravishan.c <nuvindaravishan.c@iqzsystems.com>
2023-10-10 11:52:57 +0530

    created mongodb connection to connect to mongodb database

---
+++ connection.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+MONGODB_URI = os.getenv("MONGODB_URI")
+
+def db_connection():
+    client =MongoClient(MONGODB_URI)
+
+    return client
+
