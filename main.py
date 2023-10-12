d407932db07c8855deae1a69cc68605a62951225
nuvindaravishan.c <nuvindaravishan.c@iqzsystems.com>
2023-10-12 10:36:37 +0530

    modified code to process all categories

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import asyncio
 import json
+import os
 
 from twscrape import API, gather
 from twscrape.logger import set_log_level
 from connection import db_connection
 
+import pandas as pd
+
 async def main():
     api = API()  # or API("path-to.db") - default is `accounts.db`
 
-    # ADD ACCOUNTS (for CLI usage see BELOW)
-    # await api.pool.add_account("@Tharush28383713", "Tharushaiqz12", "tharushaiqz@gmail.com", "Tharushaiqz12")
+    # ADD ACCOUNTS
+    TWITTER_USERNAME = os.getenv("TWITTER_USERNAME")
+    TWITTER_PASSWORD = os.getenv("TWITTER_PASSWORD")
+    EMAIL = os.getenv("EMAIL")
+    EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
-    await api.pool.add_account("@NuvindaC", "Nuvinda8838!", "nuvi8838@gmail.com", "Nuvinda8838!")
+    await api.pool.add_account(TWITTER_USERNAME, TWITTER_PASSWORD, EMAIL, EMAIL_PASSWORD)
 
     await api.pool.login_all()
 
@@ -19,25 +25,35 @@ async def main():
 
     db = client.twitter_data
     tweets_collection = db.tweets
-    new_tweets = list()
 
-    user_id = 44196397
 
-    for tweet in await gather(api.user_tweets(user_id, limit=20)):
-        new_tweets.append(json.loads(tweet.json()))
+    #user_id = 44196397
 
-    result = tweets_collection.insert_many(new_tweets)
-    document_ids = result.inserted_ids
-    print(f"ids of the inserted documents: {document_ids}")
+    df = pd.read_excel('twitter channels.xlsx')
+
+    df2 = df.groupby("Category")["User_ID"].apply(list).reset_index(name="user_id_list")
+
+    for i in range(len(df2)):
+        category = df2.loc[i, "Category"]
+        new_tweets = list()
+        with open(category + ".json", "a") as f:
+            for user_id in df2.loc[i, "user_id_list"]:
+                for tweet in await gather(api.user_tweets(user_id, limit=100000)):
+                    new_tweets.append(json.loads(tweet.json()))
+
+            print(len(new_tweets))
+            json.dump(new_tweets, f)
+            #f.write("\n")
+
+            # result = tweets_collection.insert_many(new_tweets)
+            # document_ids = result.inserted_ids
+            # print(f"ids of the inserted documents: {document_ids}")
     client.close()
 
     # NOTE 1: gather is a helper function to receive all data as list, FOR can be used as well:
     # async for tweet in api.user_by_id(1701675223295352832):
     #
     #     print(tweet.id, tweet.user.username, tweet.rawContent)
-
-
-
 
     set_log_level("DEBUG")
 
